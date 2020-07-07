3e80306f0b352df44f3562dbd8009d40495d4de6
Kuldeep-Attri <kuldeepsharma1312@gmail.com>
2020-07-07 20:19:35 +0900

    Created the Instance Regression Architecture

--- nets/ins_regression.py
+++ nets/ins_regression.py
@@ -1,1 +1,55 @@
 import torch
+import torch.nn as nn
+
+class insRegression(object):
+	"""
+	docstring for insRegression
+	"""
+
+	def __init__(self):
+		super(insRegression, self, num_output=2).__init__()
+
+		self.conv5x5 = nn.Conv2d(
+			in_channels = 128,
+			out_channels = 32,
+			kernel_size = 5,
+			stride = 1,
+			padding = 2,
+			dilation = 1,
+			bias = False
+		)
+		self.bn5x5 = nn.BatchNorm2d(
+			num_features = 32,
+			momentum = 1e-3
+		)
+
+		self.conv1x1 = nn.Conv2d(
+			in_channels = 32,
+			out_channels = num_output,
+			kernel_size = 1,
+			stride = 1,
+			padding = 0,
+			dilation = 1,
+			bias = False
+		) 
+		self.bn1x1 = nn.BatchNorm2d(
+			num_features = num_output,
+			momentum = 1e-3
+		)
+
+		self.relu = nn.ReLU(inplace=True)
+		
+
+		def forward(x):
+			x = self.conv5x5(x)
+			x = self.bn5x5(x)
+			x = self.relu(x)
+
+			x = self.conv1x1(x)
+			x = self.bn1x1(x)
+			x = self.relu(x)
+
+			return x
+
+
+
