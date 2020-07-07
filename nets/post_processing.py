c136c0a77778a2f67bc028ab101106186edc2fbd
Kuldeep-Attri <kuldeepsharma1312@gmail.com>
2020-07-07 21:10:32 +0900

    Started PostProcessing Methods

--- nets/post_processing.py
+++ nets/post_processing.py
@@ -1,1 +1,11 @@
 import torch
+import torch.nn as nn
+
+class postProcessing(object):
+	"""
+	docstring for postProcessing
+	"""
+	def __init__(self, arg):
+		super(postProcessing, self).__init__()
+
+		
