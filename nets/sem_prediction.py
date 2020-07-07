0750cfc621840f950ddf411f107ba658cb6bfed1
Kuldeep-Attri <kuldeepsharma1312@gmail.com>
2020-07-07 21:09:24 +0900

    Created the Semantic Predition Architecture

--- nets/sem_prediction.py
+++ nets/sem_prediction.py
@@ -1,1 +1,62 @@
 import torch
+import torch.nn as nn
+
+class semPrediction(object):
+	"""
+	docstring for semPrediction
+	"""
+
+	def __init__(self):
+		super(semPrediction, self, num_output=1000).__init__()
+
+		self.conv5x5 = nn.Conv2d(
+			in_channels = 256,
+			out_channels = 256,
+			kernel_size = 5,
+			stride = 1,
+			padding = 2,
+			dilation = 1,
+			bias = False
+		)
+		self.bn5x5 = nn.BatchNorm2d(
+			num_features = 256,
+			momentum = 1e-3
+		)
+
+		self.conv1x1 = nn.Conv2d(
+			in_channels = 256,
+			out_channels = num_output,
+			kernel_size = 1,
+			stride = 1,
+			padding = 0,
+			dilation = 1,
+			bias = False
+		) 
+		self.bn1x1 = nn.BatchNorm2d(
+			num_features = num_output,
+			momentum = 1e-3
+		)
+
+		self.relu = nn.ReLU(inplace=True)
+		
+
+		def forward(x):
+			x = self.conv5x5(x)
+			x = self.bn5x5(x)
+			x = self.relu(x)
+
+			x = self.conv1x1(x)
+			x = self.bn1x1(x)
+			x = self.relu(x)
+
+			return x
+
+
+
+
+
+
+
+
+
+		
