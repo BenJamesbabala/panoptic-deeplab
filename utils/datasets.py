e45c31f090744b49c48a76c75dfb8d4cb294ad79
Kuldeep-Attri <kuldeepsharma1312@gmail.com>
2020-07-08 14:24:45 +0900

    Added the dataset script

--- utils/datasets.py
+++ utils/datasets.py
@@ -10,3 +10,15 @@ def load_cityscape():
 	datasets.Cityscapes('./data/cityscapes', split='train', mode='fine', target_type=['semantic','instance'])
 
 	return
+
+
+class datasetCityScpape(object):
+	"""
+	docstring for datasetCityScpape
+	"""
+	def __init__(self):
+		super(datasetCityScpape, self).__init__()
+
+
+
+		self.data = load_cityscape()		
