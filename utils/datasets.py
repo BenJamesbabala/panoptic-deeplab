9712cc5797b860107ac0555324f74c5e2a9b2e6b
Kuldeep-Attri <kuldeepsharma1312@gmail.com>
2020-07-08 17:06:00 +0900

    Corrected the data loading part

--- utils/datasets.py
+++ utils/datasets.py
@@ -7,9 +7,8 @@ import torch.nn.functional as F
 
 def load_cityscape():
 
-	datasets.Cityscapes('./data/cityscapes', split='train', mode='fine', target_type=['semantic','instance'])
+	return datasets.Cityscapes('./data/cityscapes', split='train', mode='fine', target_type=['semantic','instance'])
 
-	return
 
 
 class datasetCityScape(object):
