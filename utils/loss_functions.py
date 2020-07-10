d0afaae5ba1b852ded8aa199ad3dbf5afaefa424
Kuldeep-Attri <kuldeepsharma1312@gmail.com>
2020-07-10 15:31:35 +0900

    Added both MSE and L1 loss for instanes center and offset loss

--- utils/loss_functions.py
+++ utils/loss_functions.py
@@ -1,5 +1,3 @@
-import numpy as np
-
 import torch
 import torch.nn as nn
 
@@ -17,14 +15,43 @@ def weighted_bootstrapped_CEL(predictions, labels, weight=1.0, K=0.15, N=1025*20
 
 	total_elements_in_CELoss = weighted_CELoss.numel() # Getting number of elements
 	print("number of pixels --> ", N, " from numel --> ", total_elements_in_CELoss)
-	
+
 	K_number_pixels = int(K * total_elements_in_CELoss)
 	top_K_loss_values = torch.topk(weighted_CELoss, K_number_pixels, largest=True)
 	
 	return top_K_loss_values.mean()
 
 
-def function():
-	pass
+
+def mse_loss(predicted_center, gt_center):
+	'''
+	A very simple MSE Loss between the instances centers (ground_truth vs predicted)
+	'''
+	
+	return nn.MSELoss(predicted_center, gt_center)
+
+
+
+def l1_loss(pixels_coords, center_coords):
+	'''
+	A very simple L1 Loss between the pixels offsets from the instance centers
+	'''
+	
+	return nn.L1Loss(pixels_coords, center_coords)
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
