ae344f85715e597e53406277c723a28428529609
Kuldeep-Attri <kuldeepsharma1312@gmail.com>
2020-07-22 09:30:21 +0900

    Added train_epoch method for Training

--- train.py
+++ train.py
@@ -9,7 +9,7 @@ import torch.nn as nn
 import torch.optim as optim
 
 from nets import ResnetBackbone, PSDL
-from utils import datasetCityScape, get_train_loader, get_val_loader, compute_loss
+from utils import get_train_loader, get_val_loader, compute_loss
 
 ###################################
 LOG_INTERVAL = 10
@@ -23,7 +23,7 @@ if torch.cuda.is_available():	  #
 ###################################
 
 
-def train_epoch(model, epoch, optimizer, train_data):
+def train_epoch(model, epoch, train_data_loader, optimizer):
 	
 	model.train()
 	for param in model.parameters():  # Setting complete model to be trainable
@@ -45,32 +45,31 @@ def train_epoch(model, epoch, optimizer, train_data):
 
 	print("epoch number --> " + str(epoch) + " learning rate ---> " + str(learning_rate))
 
-    train_loss = 0
-    for batch_idx, (data, target) in enumerate(train_data):
-        # Send data to the device
-        data = data.to(dev)
-        target = target.to(dev)
+	train_loss = 0
+	for batch_idx, (data, target) in enumerate(train_data_loader):
+		
+		data = data.to(dev)
+		target = target.to(dev)
 
-        optimizer.zero_grad()
+		optimizer.zero_grad()
 
-        sempred, inspred, insreg = model(data)
-        
-        loss = loss_functions(sempred, inspred, insreg, target) 
+		sempred, inspred, insreg = model(data)
+		
+		loss = compute_loss(sempred, inspred, insreg, target) 
 
-        loss.backward()
-        train_loss += loss.item()
+		loss.backward()
 
-        optimizer.step()
+		train_loss += loss.item()
 
-        if batch_idx % LOG_INTERVAL == 0:
-            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
-                epoch, batch_idx * len(data), len(train_loader.dataset),
-                100. * batch_idx / len(train_loader),
-                loss.item() / len(data)))
+		optimizer.step()
 
-    print('====> Epoch: {} Average loss: {:.4f}'.format(
-        epoch, train_loss / len(train_loader.dataset)))
+	print('====> Epoch: {} Average loss: {:.4f}'.format(epoch, train_loss))
 
+
+def val_epoch(model, epoch, val_data_loader):
+	# Need to implement...
+
+	pass
 
 
 
@@ -92,14 +91,20 @@ if __name__ == '__main__':
 	model = PSDL().to(dev)
 	print(model)
 
-	train_data = get_train_loader(args.root_dir)
-	val_data = get_val_loader(args.root_dir)
+	train_data_loader = get_train_loader(args.root_dir)
+	val_data_loader = get_val_loader(args.root_dir)
 
 	optimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0.9, weight_decay=1e-4)
 
-
 	# Training...
 	for i in range(args.num_epcohs):
+		train_epoch(model, epoch, train_data_loader, optimizer)
+		val_epochO(model, epoch, val_data_loader)
+
+	torch.save(model.state_dict(), "models/epoch" + str(args.num_epcohs) + ".pth")
+
+	print("Training Done!!!")
+
 
 
 
